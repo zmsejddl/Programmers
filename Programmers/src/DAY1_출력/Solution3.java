8661f83d943750f17c52cafe19424a5a204cd9fe
Jung Young Hun <hujung0268@gamil.com>
2024-10-01 12:47:06 +0900

    문자열 반복해서 출력하기

---
+++ Programmers/src/DAY1_출력/Solution3.java
@@ -0,0 +1,15 @@
+package DAY1_출력;
+
+import java.util.Scanner;
+
+public class Solution3 {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		String str = sc.next();
+		int n = sc.nextInt();
+		
+		for(int i=0;i<n;i++) {
+			System.out.print(str);
+		}
+	}
+}
