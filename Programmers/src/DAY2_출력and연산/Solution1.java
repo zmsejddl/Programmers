47b2a1a11042efcd0e823c53d8040fe377aa2969
Jung Young Hun <hujung0268@gamil.com>
2024-10-02 12:56:08 +0900

    덧셈식 출력하기

---
+++ Programmers/src/DAY2_출력and연산/Solution1.java
@@ -0,0 +1,13 @@
+package DAY2_출력and연산;
+
+import java.util.Scanner;
+
+public class Solution1 {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int a = sc.nextInt();
+		int b = sc.nextInt();
+
+		System.out.println(a + " + " + b + " = " + (a + b));
+	}
+}
