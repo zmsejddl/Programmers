461bcef018f23c56d034eadba4301649d42310d6
Jung Young Hun <hujung0268@gamil.com>
2024-10-02 13:03:05 +0900

    홀짝 구분하기

---
+++ Programmers/src/DAY2_출력and연산/Solution4.java
@@ -0,0 +1,15 @@
+package DAY2_출력and연산;
+
+import java.util.Scanner;
+
+public class Solution4 {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int n = sc.nextInt();
+		if (n % 2 == 0) {
+			System.out.println(n + " is even");
+		} else {
+			System.out.println(n + " is odd");
+		}
+	}
+}
