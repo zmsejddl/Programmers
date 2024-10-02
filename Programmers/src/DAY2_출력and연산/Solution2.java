0a294d137a7fdc83965ebc15e583f37807311470
Jung Young Hun <hujung0268@gamil.com>
2024-10-02 12:57:33 +0900

    문자열 붙여서 출력하기

---
+++ Programmers/src/DAY2_출력and연산/Solution2.java
@@ -0,0 +1,13 @@
+package DAY2_출력and연산;
+
+import java.util.Scanner;
+
+public class Solution2 {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		String a = sc.next();
+		String b = sc.next();
+
+		System.out.println(a + b);
+	}
+}
