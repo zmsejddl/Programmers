31d8af80c501624e2e31b1a8c046d30e6bd898bd
Jung Young Hun <hujung0268@gamil.com>
2024-10-22 10:21:54 +0900

    0 떼기 정수로 이루어진 문자열 n_str이 주어질 때, n_str의 가장 왼쪽에 처음으로 등장하는 0들을 뗀 문자열을 return하도록 solution 함수를 완성해주세요.

---
+++ Programmers/src/DAY22_함수_메섣and조건문활용/Solution1.java
@@ -0,0 +1,16 @@
+package DAY22_함수_메섣and조건문활용;
+
+public class Solution1 {
+	public static void main(String[] args) {
+		Solution12 sol = new Solution12();
+		System.out.println(sol.solution("0010"));
+		System.out.println(sol.solution("854020"));
+	}
+}
+
+class Solution12 {
+	public String solution(String n_str) {
+		String answer = "" + Integer.parseInt(n_str);
+		return answer;
+	}
+}
