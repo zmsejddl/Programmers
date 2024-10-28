ad12a8e22a25b9afe11078ace15e5fe0eb5ec2d9
Jung Young Hun <hujung0268@gamil.com>
2024-10-28 10:01:09 +0900

    [PCCE 기출문제] 1번 / 문자 출력 주어진 코드는 변수에 데이터를 저장하고 출력하는 코드입니다. 아래와 같이 출력되도록 빈칸을 채워 코드를 완성해 주세요.

---
+++ Programmers/src/DAY27/Solution2.java
@@ -0,0 +1,12 @@
+package DAY27;
+
+import java.util.Scanner;
+
+public class Solution2 {
+	public static void main(String[] args) {
+		String message = "Let's go!";
+
+		System.out.println("3\n2\n1");
+		System.out.println(message);
+	}
+}
